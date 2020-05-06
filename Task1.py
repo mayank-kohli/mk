d572f36ee356837ee2fcd76bb68328b0bc5e54e8
mayank-kohli <64058587+mayank-kohli@users.noreply.github.com>
2020-05-06 16:50:50 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+j = []
+
+for i in range (2000,3200):
+    if (i%7==0) and (i%5 != 0):
+        j.append(str(i))
+          print(j)
+
+
+# In[14]:
+
+
+i = input("first name - ")
+j = input("Last name - ")
+print(j,i)
+
+
+# In[24]:
+
+
+v = 0
+
+pi = 3.1415926535897931
+r = int(input("Enter radius = "))
+v = 4/3*pi*r
+print("volume of sphere =", v)
+
+
+# In[ ]:
+
+
+
+
